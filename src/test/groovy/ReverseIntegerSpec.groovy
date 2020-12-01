ba662674ddbb57e89d4d7fa47361d17ef9fa4fe2
Karol Teske <karol.teske@allegro.pl>
2020-12-01 06:40:59 +0100

    Day 1

---
+++ src/test/groovy/ReverseIntegerSpec.groovy
@@ -0,0 +1,24 @@
+import spock.lang.Specification
+import spock.lang.Unroll
+
+class ReverseIntegerSpec extends Specification {
+
+    def solution = new ReverseInteger()
+
+    @Unroll
+    def "should solve cases"() {
+        when:
+        def result = solution.reverse(input)
+
+        then:
+        result == expectedOutput
+
+        where:
+        input      || expectedOutput
+        123        || 321
+        0          || 0
+        -123       || -321
+        120        || 21
+        1 || 1
+    }
+}
---
+++ src/test/groovy/TwoSumSpec.groovy
@@ -0,0 +1,31 @@
+import spock.lang.Specification
+
+class TwoSumSpec extends Specification {
+
+    def solution = new TwoSum()
+
+    def "should solve simple case"(){
+        given:
+        def input = [2,7,11,15] as int[]
+        def target = 9
+
+        when:
+        def result=  solution.twoSum(input, target)
+
+        then:
+        result == [0,1] as int[]
+    }
+
+    def "should solve second case"(){
+        given:
+        def input = [3,2,4] as int[]
+        def target = 6
+
+        when:
+        def result=  solution.twoSum(input, target)
+
+        then:
+        result == [1,2] as int[]
+    }
+
+}
